355b1bd34dec0b9d4d27aa9de9bd6fdb8fb0ba04
sg <1689406287@qq.com>
2019-03-30 15:13:21 +0800

    sql generaort

---
+++ src/main/java/com/dao/EmpMapper.java
@@ -0,0 +1,17 @@
+package main.java.com.dao;
+
+import main.java.com.po.Emp;
+
+public interface EmpMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Emp record);
+
+    int insertSelective(Emp record);
+
+    Emp selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Emp record);
+
+    int updateByPrimaryKey(Emp record);
+}
